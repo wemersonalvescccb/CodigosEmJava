8d11da0669a499901dc0f00f238f964fe6e047c6
wemersonalvescccb <49341017+wemersonalvescccb@users.noreply.github.com>
2020-03-17 13:41:41 -0300

    Codigo implatado

--- src/test/java/pageObjects/ExamplePage.java
+++ src/test/java/pageObjects/ExamplePage.java
@@ -1,5 +1,166 @@
 package pageObjects;
 
-public class ExamplePage {
+import static org.junit.Assert.assertEquals;
+import static utils.Utils.driver;
 
+import java.awt.image.CropImageFilter;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ExamplePage {
+	
+	public ExamplePage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Elementos
+	
+	@FindBy(linkText = "Sign in")
+	public WebElement botaoLogin;
+	
+	@FindBy(id = "email_create")
+	public WebElement campoEmail;
+	
+	@FindBy(id = "SubmitCreate")
+	public WebElement botaoCriarConta;
+	
+	@FindBy(id = "email")
+	public WebElement validar;
+	
+	@FindBy(id = "id_gender")
+	public WebElement nomeSenhor;
+	
+	@FindBy(id = "customer_firstname")
+	public WebElement campoNome;
+	
+	@FindBy(id = "customer_lastname")
+	public WebElement campoSobrenome;
+	
+	@FindBy(id = "passwd")
+	public WebElement campoSenha;
+	
+	@FindBy(id = "\"//input[id = 'days'][value=1]\"")
+	public WebElement cliqueDia;
+	
+	@FindBy(id = "months")
+	public WebElement cliqueMes;
+	
+	@FindBy(id = "years")
+	public WebElement cliqueAno;
+	
+	@FindBy(id = "firstname")
+	public WebElement campoEndereco;
+	
+	@FindBy(id = "lastname")
+	public WebElement subEndereco;
+	
+	@FindBy(id = "company")
+	public WebElement empresa;
+	
+	@FindBy(id = "address1")
+	public WebElement cidade;
+	
+	@FindBy(id = "id_state")
+	public WebElement estado;
+	
+	@FindBy(id = "postcode")
+	public WebElement codigoPostal;
+	
+	@FindBy(id = "id_country")
+	public WebElement pais;
+	
+	@FindBy(id = "phone_mobile")
+	public WebElement celular;
+	
+	@FindBy(id = "alias")
+	public WebElement endeFuturo;
+	
+	@FindBy(id = "submitAccount")
+	public WebElement botaoSalvar;
+	
+
+	//Metodos
+	
+	public void clicarbotaoLogin() {
+		botaoLogin.click();
+	}
+	
+	public void campoEmail(String email) {
+		campoEmail.sendKeys(email);
+	}
+	
+	public void botaoConta() {
+		botaoCriarConta.click();
+	}
+	
+	public void clicarNoCampoSr() {
+		nomeSenhor.click();
+	}
+	
+	public void adicionarCampoNome(String nome) {
+		campoNome.sendKeys(nome);
+	}
+	
+	public void adicioanarSobrenome(String sobrenome) {
+		campoSobrenome.sendKeys(sobrenome);
+	}
+	
+	public void adicionarSenha(String senha) {
+		campoSenha.sendKeys(senha);
+	}
+	
+	public void adicionarDia() {
+		cliqueDia.click();
+	}
+	
+	public void adicionarMes() {
+		cliqueMes.click();
+	}
+	
+	public void adicionarAno() {
+		cliqueAno.click();
+	}
+	
+	public void adicionarEndereco(String endereco) {
+		campoEndereco.sendKeys(endereco);
+	}
+	
+	public void adicionarSubEndereco(String subEndereco1) {
+		subEndereco.sendKeys(subEndereco1);
+	}
+	
+	public void adicionarCidade(String cidade1) {
+		cidade.sendKeys(cidade1);
+	}
+	
+	public void adicionarCaixaPostal(String cidade1) {
+		codigoPostal.sendKeys(cidade1);
+	}
+	
+	public void selecionarEstado() {
+		estado.click();
+	}
+	
+	public void adicionarPais() {
+		pais.click();
+	}
+	
+	public void adicionarCelular(String celular1) {
+		celular.sendKeys(celular1);
+	}
+	
+	public void adicionarEndFuturo(String futuro1) {
+		endeFuturo.sendKeys(futuro1);
+	}
+	
+	public void botaoSalvar() {
+		botaoSalvar.click();
+	}
+	
+	
+	public void validarCriação() {
+	}
+	
 }
--- src/test/java/stepsDefinitions/ExampleSteps.java
+++ src/test/java/stepsDefinitions/ExampleSteps.java
@@ -1,5 +1,142 @@
 package stepsDefinitions;
 
-public class ExampleSteps {
+import io.cucumber.java.an.E;
+import io.cucumber.java.es.Dado;
+import io.cucumber.java.it.Quando;
+import io.cucumber.java.pt.Entao;
+import pageObjects.ExamplePage;
+import static utils.Utils.acessarSistema;
 
+public class ExampleSteps {
+	
+	ExamplePage cd = new ExamplePage();
+
+	@Dado("que acesse o sistema")
+
+	public void queAcesseOSistema() {
+		acessarSistema();
+	}
+
+	@Quando("clicar no botao Login")
+	public void clicarNoBotaoLogin() {
+		cd.clicarbotaoLogin();
+	}
+
+	@Quando("informar no campo email {string}")
+	public void informarNoCampoEmail(String string) {
+		cd.campoEmail(string);
+	}
+
+	@Quando("clicar no botao criar conta")
+	public void clicarNoBotaoCriarConta() {
+		cd.botaoConta();
+	}
+
+	@Entao("^o sistema apresenta usuario logado$")
+	public void oSistemaApresentaUsuarioLogado() throws Throwable {
+		cd.validarCriação();
+	}
+	
+	@Quando("clicar no botao senhor")
+	public void clicarNoBotaoSenhor() {
+		cd.clicarNoCampoSr();
+	}
+
+	@Quando("informar no campo nome {string}")
+	public void informarNoCampoNome(String string) {
+	    cd.adicionarCampoNome(string);
+	}
+
+	@Quando("informar no campo sobrenome {string}")
+	public void informarNoCampoSobrenome(String string) {
+	    cd.adicioanarSobrenome(string);
+	}
+
+	@Quando("informar no campo senha {string}")
+	public void informarNoCampoSenha(String string) {
+	    cd.adicionarSenha(string);
+	}
+
+	@Quando("clicar no campo data dia")
+	public void clicarNoCampoDataDia() {
+	    cd.adicionarDia();
+	}
+
+	@Quando("selecionar um dia")
+	public void selecionarUmDia() {
+	    cd.adicionarMes();
+	}
+
+	@Quando("clicar no campo data Mes")
+	public void clicarNoCampoDataMes() {
+	    cd.adicionarAno();
+	}
+
+	@Quando("selecionar um mes")
+	public void selecionarUmMes() {
+	    
+	}
+
+	@Quando("clicar no campo data ano")
+	public void clicarNoCampoDataAno() {
+	    
+	}
+
+	@Quando("selecionar o ano")
+	public void selecionarOAno() {
+	    
+	}
+	
+	@Quando("informar no campo endereco {string}")
+	public void informarNoCampoEndereco(String string) {
+	    cd.adicionarEndereco(string);
+	}
+
+	@Quando("informar no campo segundo endereco {string}")
+	public void informarNoCampoSegundoEndereco(String string) {
+	    cd.adicionarSubEndereco(string);
+	}
+
+	@Quando("informar no campo conpanhia {string}")
+	public void informarNoCampoConpanhia(String string) {
+	    
+	}
+
+	@Quando("informar no campo endereco de email {string}")
+	public void informarNoCampoEnderecoDeEmail(String string) {
+	    cd.campoEmail(string);
+	}
+
+	@Quando("informar no campo cidade {string}")
+	public void informarNoCampoCidade(String string) {
+	    cd.adicionarCidade(string);
+	}
+
+	@Quando("clicar no campo estado")
+	public void clicarNoCampoEstado() {
+	    cd.selecionarEstado();
+	}
+
+	@Quando("informar o estado desejado")
+	public void informarOEstadoDesejado() {
+	    cd.selecionarEstado();
+	}
+
+	@Quando("informar no campo telefone {string}")
+	public void informarNoCampoTelefone(String string) {
+	    cd.adicionarCelular(string);
+	}
+
+
+	@Quando("clicar no botao registrar")
+	public void clicarNoBotaoRegistrar() {
+	    cd.botaoSalvar();
+	}
+
+	@E("^informar no campo endereço altenativo \"([^\"]*)\"$")
+	public void informarNoCampoEndereçoAltenativo(String arg1) throws Throwable {
+		cd.adicionarEndFuturo(arg1);
+	}
+
+	
 }
